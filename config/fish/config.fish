6243dc09977c3b9524a37ead4f33404aee3eecf6
Noel <noel@Noels-MacBook-Pro.local>
2025-06-02 16:55:01 -0300

    update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,13 +3,13 @@ if status is-interactive
     set -x GPG_TTY $(tty)
 end
 
+set -g fish_greeting
+
 mcfly init fish | source
 zoxide init fish --cmd cd | source
 abbr --add ls eza
 abbr --add cat bat
 alias tree="eza --tree"
-alias kube-update="AWS_PROFILE=uncover aws eks update-kubeconfig --region us-east-1 --name uncover-prod-us-east-1-eks-cluster"
-alias kube-token="kube-update && aws eks get-token --cluster-name uncover-prod-us-east-1-eks-cluster | jq -r '.status.token' | wl-copy"
 
 starship init fish | source
 direnv hook fish | source
